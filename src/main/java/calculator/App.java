7a6de09d2ba9806599770ef87dfc49b8250e1b27
Robbie <robbie331@>
2024-07-24 18:31:49 +0900

    level1-4 - do while문을 통해 최소 한번 실행 후 입력값에 따라 종료할지 결정하는 코드 작성 - do while 문 밖에 String input을 선언만하고 초기화는 do while문 안에 작성 - while문 조건에 .equals()메서드를 사용하여 비교

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,45 +1,52 @@
 package calculator;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 두 정수 Scanner로 입력받기
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
-        
-        // 사칙연산 Scanner로 입력받기
-        System.out.print("사칙연산 기호를 입력하세요: "); // (+, -, *, /)
-        // charAy(index) : charAt 메서드는 매개변수로 char 타입으로 변환하고자하는 문자열(String)의 위치(index)를 받는다.
-        // Stirng 보다 char이 단일문자를 처리할 땐 메모리와 속도에서 이점이있다.
-        char sym = sc.next().charAt(0);
+        String input;
+        do{
+            // 두 정수 Scanner로 입력받기
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
 
-        // 연산을 진행 후 결과값 출력
-        // switch(val) : val값에 따라 case별 명령어를 수행
-        int result = 0;
-        switch (sym) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 == 0) {
-                    System.out.println("0으로 나눌 수 없습니다. 두 번째 숫자를 0이아닌 다른값으로 입력해 주세요/");
-                }else{
-                    result = num1 / num2;
-                }
-                break;
-            default :
-                System.out.println("사칙연산 기호를 잘못 입력하셨습니다[+, -, *, /] 4개중 하나를 입력해주세요.");
-        };
-        System.out.println("결과: " + result);
+            // 사칙연산 Scanner로 입력받기
+            System.out.print("사칙연산 기호를 입력하세요: "); // (+, -, *, /)
+            // charAy(index) : charAt 메서드는 매개변수로 char 타입으로 변환하고자하는 문자열(String)의 위치(index)를 받는다.
+            // Stirng 보다 char이 단일문자를 처리할 땐 메모리와 속도에서 이점이있다.
+            char sym = sc.next().charAt(0);
+
+            // 연산을 진행 후 결과값 출력
+            // switch(val) : val값에 따라 case별 명령어를 수행
+            int result = 0;
+            switch (sym) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("0으로 나눌 수 없습니다. 두 번째 숫자를 0이아닌 다른값으로 입력해 주세요/");
+                    } else {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    System.out.println("사칙연산 기호를 잘못 입력하셨습니다[+, -, *, /] 4개중 하나를 입력해주세요.");
+            }
+            System.out.println("결과: " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            input = sc.next();
+        }while (!input.equals("exit"));
     }
 }
