83fb22559a2a05a328d5c902b20b2532c0654140
Robbie <robbie331@>
2024-07-24 17:54:31 +0900

    level1-3 - 조건문(switch)를 사용하여 각 연산자별 명령어 구현

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -14,6 +14,22 @@ public class App {
         // 사칙연산 Scanner로 입력받기
         System.out.print("사칙연산 기호를 입력하세요: "); // (+, -, *, /)
         // charAy(index) : charAt 메서드는 매개변수로 char 타입으로 변환하고자하는 문자열(String)의 위치(index)를 받는다.
+        // Stirng 보다 char이 단일문자를 처리할 땐 메모리와 속도에서 이점이있다.
         char sym = sc.next().charAt(0);
+
+        // 연산을 진행 후 결과값 출력
+        // switch(val) : val값에 따라 case별 명령어를 수행
+        int result = 0;
+        switch (sym) {
+            case '+' : result = num1 + num2;
+                break;
+            case '-' : result = num1 - num2;
+                break;
+            case '*' : result = num1 * num2;
+                break;
+            case '/' : result = num1 / num2;
+                break;
+        }
+        System.out.println("결과: " + result);
     }
 }
